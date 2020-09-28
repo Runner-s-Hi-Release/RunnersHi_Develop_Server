dd3943cf681c26e1d757add7fe4bb33388b9f7b0
ORANZINO <hupajupa61@gmail.com>
2020-09-29 01:01:17 +0900

    [태진] update level

--- runnershi/controllers/user.js
+++ runnershi/controllers/user.js
@@ -22,13 +22,13 @@ module.exports = {
             }
             else {
                 const { token, refreshToken } = await jwt.sign(payload);
-                res.status(CODE.OK).send(util.success(CODE.OK, MSG.CREATED_USER, { accessToken: token, nickname: payload.nickname, gender: payload.gender, image: payload.image, badge: payload.badge, win: 0, lose: 0 }));
+                res.status(CODE.OK).send(util.success(CODE.OK, MSG.CREATED_USER, { accessToken: token, nickname: payload.nickname, gender: payload.gender, level: payload.level, image: payload.image, badge: payload.badge, win: 0, lose: 0 }));
             }
         }
         else {
             // 등록돼있을 때
             const { token, refreshToken } = await jwt.sign(user[0]);
-            res.status(CODE.OK).send(util.success(CODE.OK, MSG.LOGIN_SUCCESS, { accessToken: token,  nickname: user[0].nickname, gender: user[0].gender, image: user[0].image, badge: user[0].badge, win: user[0].win, lose: user[0].lose }));
+            res.status(CODE.OK).send(util.success(CODE.OK, MSG.LOGIN_SUCCESS, { accessToken: token,  nickname: user[0].nickname, gender: user[0].gender, level: user[0].level, image: user[0].image, badge: user[0].badge, win: user[0].win, lose: user[0].lose }));
         }
     }
 }
--- runnershi/models/user.js
+++ runnershi/models/user.js
@@ -8,12 +8,13 @@ const user = {
         const people = ["지은", "태영", "가은", "유림", "예린", "수현", "세곤", "채원", "태진", "세환", "가희", "민희", "영재"];
         const gender = Math.ceil(Math.random() * 2);
         const nickname = adjective[Math.floor(Math.random() * adjective.length)] + people[Math.floor(Math.random() * people.length)];
-        const questions = "?, ?, ?, ?, ?";
+        const questions = "?, ?, ?, ?, ?, ?";
+        const level = 1;
         const badge = "000000000000";
         const image = Math.ceil(Math.random() * 9); // 1 ~ 9
-        const fields = "uuid, nickname, gender, image, badge";
+        const fields = "uuid, nickname, gender, level, image, badge";
         const query = `INSERT INTO user (${fields}) VALUES (${questions})`;
-        const values = [uuid, nickname, gender, image, badge];
+        const values = [uuid, nickname, gender, level, image, badge];
         try {
             const result = await pool.queryParamArr(query, values);
             const user = {
@@ -21,6 +22,7 @@ const user = {
                 uuid: uuid,
                 nickname: nickname,
                 gender: gender,
+                level: level,
                 image: image,
                 badge: badge,
                 win: 0,
@@ -57,6 +59,7 @@ const user = {
                     uuid: uuid,
                     nickname: user_result[0].nickname,
                     gender: user_result[0].gender,
+                    level: user_result[0].level,
                     image: user_result[0].image,
                     badge: user_result[0].badge,
                     win: record_result[0].win,
